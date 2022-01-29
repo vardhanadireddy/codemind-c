30a23605c7f23620086daecbbd8c0e55993d3d41
vardhanadireddy <98031715+vardhanadireddy@users.noreply.github.com>
2022-01-29 12:32:40 +0530

    Update Cost_to_fence_the_park_outside.c

--- Cost_to_fence_the_park_outside.c
+++ Cost_to_fence_the_park_outside.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int l,b,w,c,l1,b1,t;
+    int l,b,w,c,t;
     scanf("%d%d%d%d",&l,&b,&w,&c);
-    l1=l+2*w;
-    b1=b+2*w;
-    t=((l1*b1)-(l*b))*c;
+    t=((2*(l+w)*w)+(2*(b+w)*w))*c;
     printf("%d",t);
 }
